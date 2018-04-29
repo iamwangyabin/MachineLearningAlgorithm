8ec90e5769e6bef2ed64adf7fca1bd3995c9762f
wangyabin <yabinwango@gmail.com>
2018-04-29 18:06:09 +0800

    增加了岭回归和逐步向前回归

--- Regression/regression.py
+++ Regression/regression.py
@@ -1,49 +1,138 @@
 from numpy import *
 
-#该文件由三段数字组成，中间用tab分割
+
+# 该文件由三段数字组成，中间用tab分割
 def loadDataSet(filename):
-    #把一行以tab分解成列表
-    numFeat=len(open(filename).readline().split('\t'))-1
-    dataMat=[]
-    labelMat=[]
-    fr=open(filename)
+    # 把一行以tab分解成列表
+    numFeat = len(open(filename).readline().split('\t')) - 1
+    dataMat = []
+    labelMat = []
+    fr = open(filename)
 
     for line in fr.readlines():
-        lineArr=[]
-        #strip是去换行，split是分割成列表
-        curLine=line.strip().split('\t')
+        lineArr = []
+        # strip是去换行，split是分割成列表
+        curLine = line.strip().split('\t')
         for i in range(numFeat):
             lineArr.append(float(curLine[i]))
         dataMat.append(lineArr)
         labelMat.append(float(curLine[-1]))
-    return dataMat,labelMat
+    return dataMat, labelMat
 
-#计算最佳拟合曲线，就是一个公式的实现
-def standRegres(xArr,yArr):
-    xMat=mat(xArr)
-    yMat=mat(yArr).T
-    xTx=xMat.T*xMat
-    if linalg.det(xTx)==0.0:
+
+# 计算最佳拟合曲线，就是一个公式的实现
+def standRegres(xArr, yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    xTx = xMat.T * xMat
+    if linalg.det(xTx) == 0.0:
         print("This matrix is singular ,cannot do inverse")
         return
-    ws=xTx.I*(xMat.T*yMat)
+    ws = xTx.I * (xMat.T * yMat)
     return ws
 
+
 def start():
-    xArr,yArr=loadDataSet('/home/wang/PycharmProjects/MachineLearningAlgorithm/Regression/ex0.txt')
-    ws=standRegres(xArr,yArr)
+    xArr, yArr = loadDataSet('/home/wang/PycharmProjects/MachineLearningAlgorithm/Regression/ex0.txt')
+    ws = standRegres(xArr, yArr)
     import matplotlib.pyplot as plt
-    fig=plt.figure()
-    ax=fig.add_subplot(111)
-    xMat=mat(xArr)
-    yMat=mat(yArr)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    xMat = mat(xArr)
+    yMat = mat(yArr)
     # flatten是把多维数据降成一维
-    ax.scatter(xMat[:,1].flatten().A[0],yMat.T[:,0].flatten().A[0])
-    xCopy=xMat.copy()
+    ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
+    xCopy = xMat.copy()
     xCopy.sort(0)
-    yHat=xCopy*ws
-    ax.plot(xCopy[:,1],yHat)
+    yHat = xCopy * ws
+    ax.plot(xCopy[:, 1], yHat)
     plt.show()
 
-def lwlr(testPoint,xArr,yArr,k=1.0):
-    pass
+
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    # eye生成对角矩阵
+    weights = mat(eye(m))
+
+    for j in range(m):
+        diffMat = testPoint - xMat[j, :]
+        # 高斯核函数，但是为什么||变成这个？
+        weights[j, j] = exp(diffMat * diffMat.T / (-2.0 * k ** 2))
+    xTx = xMat.T * (weights * xMat)
+    # numpy的函数，linalg包含很多线性代数的函数，det求行列式。
+    if linalg.det(xTx) == 0.0:
+       print("This matrix is sinfular ,cannot do inverse")
+       return
+    ws=xTx.I*(xMat.T*(weights*yMat))
+    return testPoint*ws
+
+def lwlrTest(testArr,xArr,yArr,k=1.0):
+    m=shape(testArr)[0]
+    yHat=zeros(m)
+    for i in range(m):
+        yHat[i]=lwlr(testArr[i],xArr,yArr,k)
+    return yHat
+
+#岭回归
+def ridgeRegres(xMat,yMat,lam=0.2):
+    xTx=xMat.T*xMat
+    denom=xTx+eye(shape(xMat)[1])*lam
+    if linalg.det(denom)==0.0:
+        print("This matrix is singular,cannot do inverse")
+        return
+    ws=denom.I*(xMat.T*yMat)
+    return ws
+
+def ridgeTest(xArr,yArr):
+    xMat=mat(xArr)
+    yMat=mat(yArr).T
+    #y以下是对数据进行标准化处理
+    #mean求平均数,axis=0对行求平均值，返回1×n的向量，axis=1就是对列。
+    yMean=mean(yMat,0)
+    yMat=yMat-yMean
+    xMeans=mean(xMat,0)
+    #求方差
+    xVar=var(xMat,0)
+    xMat=(xMat-xMeans)/xVar
+    numTestPts=30
+    #也就形成30行，n列的零矩阵
+    wMat=zeros((numTestPts,shape(xMat)[1]))
+    #形成30个lam的值，对每个特征向量算他们的权重，最后出来的是30×n的矩阵，n是特征项
+    for i in range(numTestPts):
+        ws=ridgeRegres(xMat,yMat,exp(i-10))
+        wMat[i,:]=ws.T
+    return wMat
+
+def rssError(yArr,yHatArr):
+    return ((yArr-yHatArr)**2).sum()
+
+def stageWise(xArr,yArr,eps=0.01,numIt=100):
+    xMat=mat(xArr)
+    yMat=mat(yArr).T
+    yMean=mean(yMat,0)
+    yMat=yMat-yMean
+    xMeans = mean(xMat, 0)
+    xVar = var(xMat, 0)
+    xMat = (xMat - xMeans) / xVar
+    m,n=shape(xMat)
+    returnMat=zeros((numIt,n))
+    ws=zeros((n,1))
+    wsTest=ws.copy()
+    wsMax=ws.copy()
+    for i in range(numIt):
+        print(ws.T)
+        lowestError=inf
+        for j in range(n):
+            for sign in [-1,1]:
+                wsTest=ws.copy()
+                wsTest[j]+=eps*sign
+                yTest=xMat*wsTest
+                rssE=rssError(yMat.A,yTest.A)
+                if rssE<lowestError:
+                    lowestError=rssE
+                    wsMax=wsTest
+        ws=wsMax.copy()
+        returnMat[i,:]=ws.T
+    return returnMat
